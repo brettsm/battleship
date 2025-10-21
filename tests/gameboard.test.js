cb70c78cb341e84ae4569e773f2db34666637890
Brett <brettdansmith@gmail.com>
2025-10-21 17:28:20 -0400

    test: isOccupied works, can place at 0,0

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,17 +1,24 @@
 import { Gameboard } from '../src/domain/gameboard.js';
+import { Ship } from '../src/domain/ship.js';
 
 describe('gameboard properties', () => {
     test('gameboard constructor constructs', () => {
         expect(new Gameboard()).toBeDefined();
     });
 
-    test('', () => {
-        
+    test('gameboard properties exist', () => {
+        let gb = new Gameboard();
+        expect(gb.placed).toBeInstanceOf(Map);
     });
+
 });
 
 describe('gameboard methods', () => {
-    test('can place ships at coordinates', () => {
-        expect()
+    test('can place ship without error when valid', () => {
+        let gb = new Gameboard();
+        let s = new Ship(2);
+        gb.place(s, { x: 0, y: 0 }, 'h');
+        expect(gb.isOccupied({ x: 0, y: 0 })).toBe(true);
+        expect(gb.isOccupied({ x: 1, y: 2 })).toBe(false);
     });
 });

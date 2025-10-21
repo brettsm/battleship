1afcaddc7dad2f079a34daf5c993d50e7b6abde0
Brett <brettdansmith@gmail.com>
2025-10-21 15:01:37 -0400

    feat: constructor throws on non-positive length vals

--- src/domain/ship.js
+++ src/domain/ship.js
@@ -1,5 +1,10 @@
 export class Ship {
     constructor(length) {
+        if (typeof(length) !== 'number')
+            throw new TypeError('Ship.length expects a number');
+        if (!length || length <= 0) {
+            throw new Error('ship must have positive length');
+        }
         this.length = length;
     }
 }

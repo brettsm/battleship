beafff322242c4a4e72f2041b5bd1b7ac446c290
Brett <brettdansmith@gmail.com>
2025-10-21 18:04:57 -0400

    feat: create cellsFor to return cells covered by a placed ship

--- src/domain/helpers/gameboardHelpers.js
+++ src/domain/helpers/gameboardHelpers.js
@@ -1,1 +1,7 @@
 export const key = ({ x, y }) => x*10 + y;
+
+export const cellsFor = (length, { x, y }, dir) => {
+    return Array.from({ length }, (_, i) => 
+        dir === 'h' ? { x: x + i, y: y } : { x: x, y: y + i }
+    );
+}

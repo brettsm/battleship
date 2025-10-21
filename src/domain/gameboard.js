4c9200fde5d5ce135760bcd62774ba612ac32949
Brett <brettdansmith@gmail.com>
2025-10-21 18:05:34 -0400

    feat: place maps each coord to the ship that is on it

--- src/domain/gameboard.js
+++ src/domain/gameboard.js
@@ -1,4 +1,4 @@
-import { key } from "./helpers/gameboardHelpers.js";
+import { key, cellsFor } from "./helpers/gameboardHelpers.js";
 
 // 10x10 board
 const x = 0, y = 1;
@@ -9,9 +9,13 @@ export class Gameboard {
         
     }
 
-
+    // h for horizontal v for vertical
     place(ship, { x, y }, dir = 'h') {
-        this.#placed.set(key({ x: x, y: y}), ship);
+        let cells = cellsFor(ship.length, {x: x, y: y }, dir);
+        for (const { x, y } of cells) {
+            const k = key({ x, y });
+            this.#placed.set(k, ship);
+        }
     }
 
     isOccupied({ x, y }) {

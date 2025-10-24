bf621d8472bf5e9e79de1b151172b83c32ae63b6
Brett <brettdansmith@gmail.com>
2025-10-24 12:54:57 -0400

    feat: fix instance of gameController

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { GameController } from "./app/gameController.js";
 
 const appRoot = document.getElementById('app');
 
-const game = new GameController(appRoot);
+const game = new GameController({ appRoot: appRoot });
 game.init();
 

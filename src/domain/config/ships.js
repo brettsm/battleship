48aaba9e4866fd1feb536c85c27c999535f048d3
Brett <brettdansmith@gmail.com>
2025-10-24 16:59:27 -0400

    feat: create template for types of ships, and can get ships by id

---
+++ src/domain/config/ships.js
@@ -0,0 +1,12 @@
+export const SHIP_TYPES = Object.freeze([
+    {   id: 'aircraft-carrier',     name: 'Aircraft Carrier',   length: 5 },
+    {   id: 'battleship',           name: 'Battleship',         length: 4 },
+    {   id: 'cruiser',              name: 'Cruiser',            length: 3 },
+    {   id: 'submarine',            name: 'Submarine',          length: 2 },
+    {   id: 'destroyer',            name: 'Destroyer',          length: 2 }
+]);
+
+export const SHIP_BY_ID = Object.freeze([
+    Object.fromEntries(SHIP_TYPES.map((piece) => [piece.id, piece]))
+]);
+   

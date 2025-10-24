d310495e2ae0096111573d4528abe7417e27bbb7
Brett <brettdansmith@gmail.com>
2025-10-24 12:36:52 -0400

    feat and refactor: change constructor args and create init

--- src/app/gameController.js
+++ src/app/gameController.js
@@ -1,7 +1,24 @@
+import { Player } from "../domain/player.js";
+import { UserInterface } from "../ui/ui.js";
+
 export class GameController {
-    constructor(p1, p2, ui) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.ui = ui;
+    #userPlayer
+    #computerPlayer
+    constructor({ appRoot, rng = Math.random }) {
+        this.ui = new UserInterface(appRoot);
+        this.rng = rng;
+    }
+
+    init() {
+        this.ui.renderForm((playerName) => {
+            this.#userPlayer = new Player(playerName);
+            this.#computerPlayer = new Player();
+
+            this._startGame(this.#userPlayer, this.#computerPlayer);
+        })
+    }
+
+    _startGame(player1, player2) {
+        
     }
 }

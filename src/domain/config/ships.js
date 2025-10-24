50bcdc2f135bf486226da06dcd954a60914b82ad
Brett <brettdansmith@gmail.com>
2025-10-24 17:45:09 -0400

    feat: change length of submarine from 2 to 3

--- src/domain/config/ships.js
+++ src/domain/config/ships.js
@@ -2,7 +2,7 @@ export const SHIP_TYPES = Object.freeze([
     {   id: 'aircraft-carrier',     name: 'Aircraft Carrier',   length: 5 },
     {   id: 'battleship',           name: 'Battleship',         length: 4 },
     {   id: 'cruiser',              name: 'Cruiser',            length: 3 },
-    {   id: 'submarine',            name: 'Submarine',          length: 2 },
+    {   id: 'submarine',            name: 'Submarine',          length: 3 },
     {   id: 'destroyer',            name: 'Destroyer',          length: 2 }
 ]);
 

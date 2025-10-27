27f67b5d8eaf0967f1cc9f47fd4029be9eaf322c
Brett <brettdansmith@gmail.com>
2025-10-27 11:41:05 -0400

    feat: create _sleep(ms) returns a promise

--- src/app/gameController.js
+++ src/app/gameController.js
@@ -15,21 +15,26 @@ export class GameController {
     }
 
     init() {
-        this.ui.renderStartForm((playerName) => {
+        this.ui.renderStartForm(async (playerName) => {
             this.#userPlayer = new Player(new Gameboard(), playerName);
             this.#computerPlayer = new Player(new Gameboard(), 'Computer');
+
+            await this._computerPlacementPhase();
+            //this._placeUserShips();
+
+            // TODO: should I have it flip a coin here?
 
             const humanStarts = this._flipCoin();
             this.#current = humanStarts ? this.#userPlayer : this.#computerPlayer;
             this.#other = humanStarts ? this.#computerPlayer : this.#userPlayer;
 
+            
+            
             const message = this.current === this.#userPlayer ? 'You start' : 'Computer starts';
 
             this.ui.renderCoinFlipResult({ message: message, onDone: () => this._startGame() });
 
-            this._startGame();
-
-            //TODO: refactor like this:
+            // TODO: refactor like this:
             //      1. set user player
             //      2. set computer player
             //      3. this._placeHumanFleet();
@@ -42,11 +47,22 @@ export class GameController {
 
     _startGame() {
         this.#state = 'inPlay';
-        // TODO: now we need to choose the ships
-        this.#computerPlayer.randomizeFleet();
     }
 
     _gameIsOver() {
         return this.#userPlayer.allSunk() || this.#computerPlayer.allSunk();
+    }
+    _placeComputerShips() {
+        this.#computerPlayer.randomizeFleet();
+    }
+
+    async _computerPlacementPhase() {
+        this.ui.showBusy('Deploying enemy fleet...');
+        await this._sleep(400).then(console.log('slept'));
+
+    }
+
+    async _sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
     }
 }

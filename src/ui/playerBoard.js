644528fe0e07042c808e0388fc858890ffae3ae2
Brett <brettdansmith@gmail.com>
2025-11-03 15:34:59 -0500

    refactor: playerBoard.js now handles playerBoard construction and manip

---
+++ src/ui/playerBoard.js
@@ -0,0 +1,14 @@
+export function createPlayerBoard({
+    ids = {
+        missBoard: 'miss-board',
+        placementBoard: 'placement-board'
+    }
+} = {}) {
+    const missBoard = document.createElement('div');
+    missBoard.id = ids.missBoard;
+
+    const placementBoard = document.createElement('div');
+    placementBoard.id = ids.placementBoard;
+
+    return{ missBoard, placementBoard };
+}
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -2,35 +2,9 @@
 //          delete ui.js, import everything into gameController.js
 
 export class UserInterface {
-    #statusBar; #stage; #playerPanel; #playerBoard; #missBoard; #placementBoard;
 
     constructor(appRoot) {
         this.appRoot = appRoot;
-    }
-    
-    _makePlayerBoard() {
-        const container = document.createElement('div');
-        container.id = 'player-board';
-
-        const missBoard = document.createElement('div');
-        missBoard.id = 'miss-board';
-        this.#missBoard = missBoard;
-
-        const placementBoard = document.createElement('div');
-        placementBoard.id = 'placement-board';
-        this.#placementBoard = placementBoard;
-
-        container.appendChild(missBoard);
-        container.appendChild(placementBoard);
-
-        return container;
-
-    }
-
-    renderCoinFlipResult({ message, onDone }) {
-        this.updateStatusBar('Starting game... ' + message);
-        this.appRoot.appendChild(startMessage);
-        setTimeout(onDone, 1000);
     }
 
     renderShips() {
